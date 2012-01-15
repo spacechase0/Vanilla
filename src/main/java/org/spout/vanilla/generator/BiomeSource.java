607a87a57cadb2ad8adf247369ad879b622e8c3a
narrowtux <narrow.m@gmail.com>
2012-01-15 17:38:55 +0100

    Try out selection

--- src/main/java/org/spout/vanilla/generator/BiomeSource.java
+++ src/main/java/org/spout/vanilla/generator/BiomeSource.java
@@ -2,7 +2,7 @@ package org.spout.vanilla.generator;
 
 import java.util.ArrayList;
 
-import net.royawesome.jlibnoise.module.source.Perlin;
+import net.royawesome.jlibnoise.module.combiner.Select;
 import net.royawesome.jlibnoise.module.source.Voronoi;
 
 import org.spout.api.geo.World;
@@ -14,27 +14,39 @@ public class BiomeSource {
 	
 	public static final int SEED_SHIFT = 3982462;
 	protected Voronoi biomeType = new Voronoi();
+	protected Select finalTerrain;
 	
 	public BiomeSource() {
 		biomeType.setFrequency(0.5);
+		setupSelect();
+	}
+	
+	private void setupSelect() {
+		finalTerrain = new Select();
+		finalTerrain.setControlModule(biomeType);
+		finalTerrain.setEdgeFalloff(0.25);
 	}
 	
 	public void clearBiomes() {
 		biomes.clear();
+		setupSelect();
 	}
 
 	public void addBiome(AbstractBiome biome) {
 		biomes.add(biome);
+		finalTerrain.SetSourceModule(biomes.size() - 1, biome);
 	}
 
 	public AbstractBiome getBiomeAt(World world, int x, int z) {
 		biomeType.setSeed((int) (world.getSeed() + SEED_SHIFT));
-		double value = (biomeType.GetValue(x / 64.0, 0.01, z / 64.0) + 1.0) * biomes.size() / 2.0 - 1.0;
+		double value = (biomeType.GetValue(x / 64.0, 0.01, z / 64.0) + 1.0) * biomes.size() / 2.0;
 		return biomes.get((int) value);
 	}
 	
 	public int getHeightAt(World world, int x, int z) {
-		AbstractBiome biome = getBiomeAt(world, x, z);
-		return biome.getHeightAt(world, x, z);
+		for(AbstractBiome biome : biomes) {
+			biome.setWorld(world);
+		}
+		return (int) (finalTerrain.GetValue(x / 16.0, 0.01, z / 16.0));
 	}
 }
--- src/main/java/org/spout/vanilla/generator/biome/AbstractBiome.java
+++ src/main/java/org/spout/vanilla/generator/biome/AbstractBiome.java
@@ -1,12 +1,40 @@
 package org.spout.vanilla.generator.biome;
 
+import net.royawesome.jlibnoise.module.Module;
+
 import org.spout.api.geo.World;
 import org.spout.api.util.cuboid.CuboidShortBuffer;
 
-public abstract class AbstractBiome {
+public abstract class AbstractBiome extends Module {
+	private World world;
+	
+	public void setWorld(World world) {
+		this.world = world;
+	}
+	
+	@Override
+	public int GetSourceModuleCount() {
+		return 0;
+	}
+
+	@Override
+	public double GetValue(double x, double y, double z) {
+		return getHeightAt(world, (int) x, (int) z);
+	}
+
+	@Override
+	public void SetSourceModule(int index, Module sourceModule) {
+	}
+
+	@Override
+	public Module getSourceModule(int index) {
+		return null;
+	}
+
 	private String name = "AbstractBiome";
 	
 	public AbstractBiome(String name) {
+		super(0);
 		this.name = name;
 	}
 	
